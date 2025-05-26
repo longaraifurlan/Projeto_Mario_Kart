21481bf7547b9f68f5550ad057ccd6f69bbee9a7
Diego Longarai Furlan <longarai.furlan@gmail.com>
2025-05-26 14:14:47 +0100

    Dinamica (aperfeiçoamento)

--- src/index.js
+++ src/index.js
@@ -22,8 +22,65 @@ async function rollDice(){
     return Math.floor(Math.random() * 6) + 1
 }
 
+async function getRandomBlock() {
+    let random = Math.random()
+    let result
+
+    switch (true) {
+        case random <= 0.33:
+            result = "RETA"
+            break;
+        case random <= 0.66:
+            result = "CURVA"
+            break;
+        default:
+            result = "COMBATE"
+            break;
+    }
+
+    return result
+}
+
+async function logRollResult(characterName, block, diceResult, attribute) {
+    console.log(`${characterName} 🎲 rolou o dado de ${block} ${diceResult}`)
+        
+}
+
 async function playRaceEngine(character1, character2) {
-    
+    for (let round = 1; round <= 5; round++){
+        console.log(`🏁      Rodada ${round}      🏁`)
+
+        // Sorteio tipo de disputa
+        let block = await getRandomBlock()
+        console.log(`Disputa: ${block}`)
+    }
+    // Roll Dice
+    let diceResult1 = await rollDice()
+    let diceResult2 = await rollDice()
+
+    // habillity test
+    let totalTestSkill1 = 0
+    let totalTestSkill2 = 0
+
+    if (block === "RETA") {
+        totalTestSkill1 = diceResult1 + character1.VELOCIDADE
+        totalTestSkill2 = diceResult2 + character2.VELOCIDADE
+
+        await logRollResult(character1, "velocidade", diceResult1, character1.VELOCIDADE)
+        await logRollResult(character2, "velocidade", diceResult2, character2.VELOCIDADE)
+
+    }
+    if (block === "CURVA") {
+        totalTestSkill1 = diceResult1 + character1.MANOBRABILIDADE
+        totalTestSkill2 = diceResult2 + character2.MANOBRABILIDADE
+
+        await logRollResult(character1, "monobrabilidade", diceResult1, character1.MANOBRABILIDADE)
+        await logRollResult(character2, "manobrabilidade", diceResult2, character2.MANOBRABILIDADE)
+    }
+    if (block === "COMBATE") {
+        let powerResult1 = diceResult1 + character1.PODER
+        let powerResult2 = diceResult2 + character2.PODER
+    }
 }
 
 (async function main() {
